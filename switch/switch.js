31b00a46423cc56426146f39d2d4e6533d872448
aleksandar <aleksandarrakic@yahoo.com>
2022-10-22 17:39:19 +0200

    second commit

--- switch/switch.js
+++ switch/switch.js
@@ -105,7 +105,7 @@ switch (day) {
 }
 console.log(result)
 
-/* 4.4. Write a program that for a 1-12 number input (representing a month in a year) shows that
+/* 4. Write a program that for a 1-12 number input (representing a month in a year) shows that
 month’s name. All other cases output a message explaining that input must be a number
 between 1 and 12.
 For input 2, output should be “February”.
@@ -156,3 +156,71 @@ switch (month ) {
 }
 console.log(result)
 
+/* 5. Write a program that for a 1-12 number input (representing a month in a year) shows what
+season it is. All inputs different from 1-12 output a message explaining that input must be a
+number between 1 and 12. */
+
+var month = 7;
+result = "";
+switch (month) {
+    case 12:
+        result = "winter";
+    case 1:
+        result = "winter";
+    case 2:
+        result = "winter";
+        break;
+    case 3:
+        result = "spring";
+    case 4:
+        result = "spring";
+    case 5:
+        result = "spring";
+        break;
+    case 6:
+        result = "summer";
+    case 7:
+        result = "summer";
+    case 8:
+        result = "summer";
+        break;
+    case 9:
+        result = "outumn";
+    case 10:
+        result = "outumn";
+    case 11:
+        result = "outumn";
+        break;
+        default:
+            result = "Input must be a number betwen 1-12"
+}
+console.log( result )
+
+/* 6. Write a program that for a string input of a grade from “A”-“F” outputs a proper info
+message about that grade in the following manner: A - &quot;Good job&quot;&quot;, B - &quot;Pretty good&quot;&quot;, C -
+&quot;Passed&quot;&quot;, D - &quot;Not so good&quot;&quot;, F - &quot;Failed&quot;. Input different from letters A-F outputs a message
+&quot;Unknown grade&quot;. */
+
+
+var grade = "A";
+result = "";
+switch ( grade ) {
+    case "A":
+        result = "Good job";
+        break;
+    case "B":
+        result = "Pretty good";
+        break;
+    case "C":
+        result = "Passed";
+        break;
+    case "D":
+        result = "Not so good";
+        break;
+    case "E":
+        result = "F";
+        break;
+        default:
+            result = "Unkown"
+}
+console.log(result)
