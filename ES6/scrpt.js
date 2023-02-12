b20185e3cd9c9b276fda484f78269cde1ad95e48
aleksandar <aleksandarrakic@yahoo.com>
2023-02-12 23:58:06 +0100

    es6 tasks

--- ES6/scrpt.js
+++ ES6/scrpt.js
@@ -1,6 +1,77 @@
+// 3. Write a function that increases each element of the given array by the given value. If the value is
+// omitted, increase each element of the array by 1.
+// Input: [4, 6, 11, -9, 2.1], 2
+// Output: [6, 8, 13, -7, 4.1]
+
+
+
+
+// 4. Write a function that filters all even elements of the array.
+// Input: [6, 11, 9, 0, 3]
+// Output: [6, 0]
+
+
+const arr12 = [6, 11, 9, 0, 3];
+
+const filterarr12 = arr12.filter(e => e % 2 === 0);
+console.log(filterarr12);
+
+// 5. Write a function that filters all the strings from the given array that contain substring JS or js.
+// Input: ["compiler", "transpiler", "babel.js", "JS standard", "linter"]
+// Output: ["babel.js", "JS standard"]
+
+
+const arr42 = ["compiler", "transpiler", "babel.js", "JS standard"];
+
+const filterarr42 = arr42.filter(e => e.includes("js") || e.includes("JS"));
+
+console.log(filterarr42);
+
+
+
+
+/* 6. Write a function that filters all integer numbers from the given array. 
+Input: [1.6, 11.34, 9.23, 7, 3.11, 8]
+    Output: [7, 8] */
+
+
+const arrInput = [1.6, 11.34, 9.23, 7, 3.11, 8];
+
+const filterIntegger = arrInput.filter(e => Number.isInteger(e));
+
+console.log(filterIntegger);
+
+
+// 7. Write a function that filters all integer arguments that contain digit 5.
+// Function arguments: 23, 11.5, 9, "abc", 45, 28, 553
+// Output: [45, 553]
+
+const arg = [23, 11.5, 9, "abc", 45, 28, 553];
+
+const filterArguments = arg.filter(e => `${e}`.includes("5") && !`${e}`.includes(".5"));
+
+console.log(filterArguments);
+
+
+
 // 8. Write a function that returns indexes of the elements greater than 10.
 // Input: [1.6, 11.34, 29.23, 7, 3.11, 18]
 // Output: [1, 2, 5]
+
+const arr1 = [1.6, 11.34, 29.23, 7, 3.11, 18];
+
+const greaterThan10 = arr => {
+    const result = [];
+    arr.forEach((value, index) => {
+        if (value > 10) {
+            result.push(index);
+        };
+    });
+    return result;
+};
+
+
+console.log(greaterThan10(arr1));
 
 
 
