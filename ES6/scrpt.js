7686ed3f7f293b1853c31c26985b1ddddcc86f5b
aleksandar <aleksandarrakic@yahoo.com>
2023-02-13 01:22:40 +0100

    es6 done

--- ES6/scrpt.js
+++ ES6/scrpt.js
@@ -1,8 +1,51 @@
+// 1. Write a function that capitalizes the first letter of each str argument it receives.
+// Function arguments: [&#39;hello&#39;, &#39;there&#39;, &#39;ES&#39;, 6]
+// Output: [&#39;Hello&#39;, &#39;There&#39;, &#39;ES&#39;]
+
+
+
+const arr6 = ["hello", "there", "ES", 6];
+const capitalize = arr6.filter(e => typeof e === "string").map(e => e[0].toUpperCase() + e.slice(1));
+console.log(capitalize);
+
+
+
+// 2. Write a function that calculates sale tax that should be paid for the product of the given price.
+// Use a constant to set a fixed value of the tax rate (i.e. 20% in Serbia).
+// Input: [{ name: “Banana”, price: 120 }, {name: “Orange”, price: 100}]
+// Output: [{ name: “Banana”, price: 144 }, { name: “Orange”, price: 120 }] // product full price
+// Output2: [ 24, 20 ] // tax only
+
+const products = [
+    { name: "Banana", price: 120 },
+    { name: "Orange", price: 100 }
+];
+
+const taxRate = 0.2;
+
+const productFullPrice = products.map(e => {
+    return {
+        name: e.name,
+        price: e.price * (1 + taxRate)
+    };
+});
+console.log(productFullPrice);
+
+const taxOnly = products.map(e => e.price * taxRate);
+console.log(taxOnly);
+
+
+
 // 3. Write a function that increases each element of the given array by the given value. If the value is
 // omitted, increase each element of the array by 1.
 // Input: [4, 6, 11, -9, 2.1], 2
 // Output: [6, 8, 13, -7, 4.1]
 
+
+const arr11 = [4, 6, 11, -9, 2.1];
+
+const increasearr11 = arr11.map(e => e + 2);
+console.log(increasearr11);
 
 
 
