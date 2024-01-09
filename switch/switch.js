462335ab0243a46ba27a06b33d706f01a6d5c45a
aleksandar <aleksandarrakic@yahoo.com>
2024-01-09 13:08:26 +0100

    new

--- switch/switch.js
+++ switch/switch.js
@@ -291,3 +291,35 @@ switch ( operation ) {
         console.log( a / b )
         break;
 }
+
+/* 9. Write a switch statement which tests val and sets answer for the following conditions:
+1 - alpha
+2 - beta
+3 - gamma
+4 - delta */
+
+function switchOptions(val) {
+
+    let answer = "";
+
+    switch(val) {
+        case 1:
+        answer = "alpha";
+        break;
+
+        case 2: 
+        answer = "beta";
+        break;
+
+        case 3: 
+        answer = "gamma";
+        break;
+
+        case 4: 
+        answer = "delta";
+        break;
+    }
+    return answer;
+}
+
+console.log(switchOptions(1));
